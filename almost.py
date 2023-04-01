6041629b3a04d4d60bf4c796810b84b47e6e9172
kenneththomas <kennethwoo11@gmail.com>
2023-04-01 11:52:07 -0400

    fstrings and fix the multiple calls to api

--- almost.py
+++ almost.py
@@ -1,17 +1,15 @@
 import requests
 import datetime
 
-#yesterday's date in the format YYYY-MM-DD
+# yesterday's date in the format YYYY-MM-DD
 date = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
 
 
 def find_atd(data):
     for player in data['data']:
-
         sevcount = 0
         ddcount = 0
 
-        # print(player)
         points = player['pts']
         blocks = player['blk']
         assists = player['ast']
@@ -27,35 +25,28 @@ def find_atd(data):
                 ddcount += 1
 
         if sevcount >= 3:
-            date = player['game']['date']
-            date = date.replace('T00:00:00.000Z', '')
-            firstname = player['player']['first_name']
-            lastname = player['player']['last_name']
-            fullname = firstname + ' ' + lastname
+            date = player['game']['date'].replace('T00:00:00.000Z', '')
+            fullname = f"{player['player']['first_name']} {player['player']['last_name']}"
+
             if ddcount <= 2:
-                print('{} {} {}pts/{}reb/{}ast/{}blk/{}stl'.format(fullname, date, points, rebounds, assists, blocks,
-                                                                   steals))
+                print(f"{fullname} {date} {points}pts/{rebounds}reb/{assists}ast/{blocks}blk/{steals}stl")
             else:
-                print('{} got an actual triple double'.format(fullname))
+                print(f"{fullname} got an actual triple double")
+
 
 def main():
+    url = f"https://www.balldontlie.io/api/v1/stats?dates[]={date}&per_page=100&page="
 
-    x = requests.get('https://www.balldontlie.io/api/v1/stats?dates[]={}&per_page=100&page=1'.format(date))
-    data = x.json()
+    response = requests.get(url + "1")
+    data = response.json()
     pages = data['meta']['total_pages']
 
-    '''
-    todo: under this logic we make the page 1 request twice
-    the first time is to find out how many total pages there are
-    the second time is to actually perform the calculations
-    ideally we don't repeat that first request however the rate limit is like 60/min
-    we should not come close to hitting that
-    '''
-   
-    for pageno in range(1,pages):
-        x = requests.get('https://www.balldontlie.io/api/v1/stats?dates[]={}&per_page=100&page={}'.format(date, pageno))
-        data = x.json()
+    find_atd(data)
+
+    for pageno in range(2, pages + 1):
+        response = requests.get(url + str(pageno))
+        data = response.json()
         find_atd(data)
 
-main()
 
+main()
